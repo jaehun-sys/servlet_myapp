dd7fcdc8e6194c73073c490ba715ec1edc3ffe3a
jaehun-sys <us03098@gmail.com>
2021-12-30 15:11:24 +0900

    2021-12-30-v1 열심히 이메일 dao 만드는중

---
+++ src/main/java/ch11/DriverTest.java
@@ -0,0 +1,21 @@
+package ch11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverTest{
+	public static void main(String args[]){
+		Connection con;   
+
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			con = DriverManager.getConnection(url, "webdb", "webdb12");
+      //con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "1234");
+			System.out.println("Success");
+		}
+		catch(SQLException ex){ System.out.println("SQLException" + ex);}
+		catch(Exception ex){ System.out.println("Exception:" + ex);}
+	}
+}
